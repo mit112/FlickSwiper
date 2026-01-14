f5530d3b75d335d3a38d0d26ec5fe10035558a08
MiTsheth08 <sheth.mit@northeastern.edu>
2026-01-14 10:59:59 -0600

    Initial Commit

---
+++ AlreadySeen/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AlreadySeen
+//
+//  Created by MiT on 1/14/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
